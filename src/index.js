b51580d9567ed293ae59bd74035d98d1bb00aa00
AstasiaG <anstasa.li@gmail.com>
2023-07-26 18:46:09 +0400

    feat: add morse-decode function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = expr.match(/.{10}/g); //делит строку на кол-во символов и возвращает массив
+    let res = '';
+    let sym = '';
+
+    arr.forEach(e => {
+        if (e === '**********') {
+            res += ' ';
+        } else {
+            let elem = e.match(/../g);
+            for(let i = 0; i < elem.length; i++) {
+                if(elem[i] === '10') {
+                sym += '.';
+                } else if (elem[i] === '11') {
+                sym += '-';
+                }
+            }
+        }
+
+        for(let prop in MORSE_TABLE){
+            if(prop === sym) {
+            res += MORSE_TABLE[prop];
+            }
+        }
+        sym = '';
+    });
+
+    return res;
 }
 
 module.exports = {
